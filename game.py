e949a4809735173ba230ae636b17f28c0ca9bca7
MohamedEssam <mo849026@gmail.com>
2023-05-20 18:47:47 +0300

    implementing alpha beta pruning algorithm

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 from board import Board
 import time
@@ -33,7 +35,7 @@ def main():
         if (algorithm == MINIMAX):
             column = minimax(np.array(game_board), difficulty, True)[0]
         else:
-            pass
+            column = alpha_beta_pruning(np.array(game_board),difficulty,-math.inf,math.inf,True)[0]
 
         # Insert here the action you want to perform based on the output of the algorithm
         # You can use the following function to select a column
@@ -85,6 +87,56 @@ def minimax(board, depth, maximizing_player):
                 score = new_score
                 column = col
         return column, score
+
+def alpha_beta_pruning(board, depth, alpha, beta, maximizing_player):
+
+    valid_moves = get_valid_moves(board)
+    is_terminal = is_terminal_node(board)
+
+    if depth == 0 or is_terminal:
+        if is_terminal:
+            if score_position(board, AI_AGENT) == 100:
+                return None, 100000000000000
+            elif score_position(board, COMPUTER) == 100:
+                return None, -10000000000000
+            else:
+                return None, 0
+        else:
+            return None, score_position(board, AI_AGENT)
+
+    if maximizing_player:
+        score = -np.Inf
+        # print("valid_moves: ", valid_moves)
+        column = np.random.choice(valid_moves)
+        for col in valid_moves:
+            row = get_next_open_row(board, col)
+            copy_board = board.copy()
+            move(copy_board, row, col, AI_AGENT)
+            new_score = alpha_beta_pruning(copy_board, depth - 1, alpha, beta, False)[1]
+            if new_score > score:
+                score = new_score
+                column = col
+            alpha = max(alpha, score)
+            if alpha >= beta:
+                break
+        return column, score
+    else:
+        score = np.Inf
+        # print("valid_moves: ", valid_moves)
+        column = np.random.choice(valid_moves)
+        for col in valid_moves:
+            row = get_next_open_row(board, col)
+            copy_board = board.copy()
+            move(copy_board, row, col, COMPUTER)
+            new_score = alpha_beta_pruning(copy_board, depth - 1, alpha, beta, True)[1]
+            if new_score < score:
+                score = new_score
+                column = col
+            beta = min(beta, score)
+            if alpha >= beta:
+                break
+        return column,score
+
 
 
 def get_valid_moves(board):
@@ -166,3 +218,5 @@ def move(board, row, col, player):
 
 if __name__ == "__main__":
     main()
+
+
